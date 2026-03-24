3d9d792001075aaf3a224dbc7f64a61f7fd264de
Subroto Roy <subrotoroyss288@gmail.com>
2026-03-24 23:19:39 +0600

    registration and login functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -27,9 +27,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/',blogView.blogs, name='blogs'),
-
-    #search endpoint
     path('blogs/search/', blogView.search, name='search'),
+    path('blogs/<slug:slug>/',blogView.blogs, name='blogs'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,8 +1,13 @@
 
-from django.shortcuts import render
+import django
+from django.shortcuts import redirect, render
 
 from assignments.models import About
 from blogs.models import blog, Category 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     featured_posts = blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
@@ -20,3 +25,43 @@ def home(request):
 
     }
     return render(request, 'home.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+
+         form  = RegistrationForm()
+    context ={
+        'form': form,
+    }
+    return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
